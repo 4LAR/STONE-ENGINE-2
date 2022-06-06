32f4fa3217f109a9839fe7579e28353de07786f7
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-06-06 22:38:47 +0300

    сохранение кода в файл

--- fuckimport/fuckimport.py
+++ fuckimport/fuckimport.py
@@ -5,16 +5,36 @@
 
 import os
 import pickle
+import hashlib
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+def get_hash(text):
+    return hashlib.md5(text.encode('utf-8')).digest()
+
+# класс для хранения всего кода в файл
+class file_code():
+    def __init__(self):
+        self.files = []
+        self.code = ''
+        self.hash = ''
 
 class fuck_import():
     def __init__(self, path='objects'):
 
-        self.path = path
-        self.main_file_name = ''
+        self.path = path         # дтректория где будут лежать файлы
+        self.main_file_name = '' # название основного списка
 
-        self.code = ''
-        self.files = []
+        self.code = ''  # здесь будет храниться код после сборки (build)
+        self.files = [] # здесь будет храниться список файлов и размер каждого файла в строках
 
+    # чтение списка файлов проекта
     def read(self, name='objects'):
         file_objects = (
             open(self.path + '/' + name + '.list', 'r', encoding="utf-8").read()
@@ -27,10 +47,48 @@ class fuck_import():
                 else:
                     self.files.append(
                         [
-                            file_name,
-                            0
+                            (name + '.list').replace( # название и путь у файлу
+                                name.split('/')[len(name.split('/'))-1] + '.list',
+                                file_name
+                            ),
+                            0 # количество строк в файле (по умалочанию 0)
                         ]
                     )
 
+    # сборка всех файлов из списка в код
+    def build(self):
+        self.code = ''
+        for file_code_name in self.files:
+            try:
+                file_code_buf = open(self.path + '/' + file_code_name[0], 'r', encoding="utf-8").read() + '\n'
+                file_code_name[1] = len(file_code_buf.split('\n'))
+                self.code += file_code_buf
+
+            except:
+                print('gg')
+
+    # функция для сохранения всего кода в 1 собранный файл
+    def pack(self, name):
+        file_code_buf = file_code()
+        file_code_buf.files = self.files
+        file_code_buf.code = self.code
+        file_code_buf.hash = get_hash(self.code)
+
+        save_obj(file_code_buf, name)
+
+    # функция для чтения собранного файла
+    def read_pack(self, name):
+        file_code_buf = load_obj(name)
+
+        if (get_hash(file_code_buf.code) == file_code_buf.hash):
+            self.code = file_code_buf.code
+            self.files = file_code_buf.files
+
+            return True
+        else:
+            print('error hash')
+            return False
+
+    # возврат кода
     def get_code(self):
         return self.code
--- fuckimport/main.py
+++ fuckimport/main.py
@@ -2,4 +2,7 @@ from fuckimport import *
 
 fuck_import = fuck_import()
 fuck_import.read()
-eval(fuck_import.get_code())
+fuck_import.build()
+#exec(fuck_import.get_code())
+#fuck_import.pack('data')
+fuck_import.read_pack('data')
